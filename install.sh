9ade256a82c8aa5c18bde7abcf54403712aa1769
Antonio Galea <antonio.galea@gmail.com>
2015-10-27 11:01:47 +0100

    use own version of tg

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ me=$BASH_SOURCE
 while [ -L "$me" ]; do me=$(file -- "$me"|cut -f2 -d\`|cut -f1 -d\'); done
 #'
 BASE="`cd -P -- "$(dirname -- "$me")" && pwd -P`"
-cd $BASE
+cd "$BASE"
 
 virtualenv env
 . env/bin/activate
@@ -13,12 +13,12 @@ pip install -U pip
 pip install bottle-beaker bottle-cork bottle-websocket pyOpenSSL
 pip install DictObject
 pip install git+https://github.com/luckydonald/pytg.git
-git clone --recursive https://github.com/vysheng/tg.git
+git clone --recursive https://github.com/ant9000/tg.git
 cd tg
 sudo apt-get install \
     build-essential make libreadline-dev libconfig-dev libssl-dev \
     lua5.2 liblua5.2-dev libevent-dev libjansson-dev python-dev
-./configure && make && bin/telegram-cli
+./configure && make && TELEGRAM_HOME="$BASE" bin/telegram-cli
 
 cat<<MSG
 ############################
